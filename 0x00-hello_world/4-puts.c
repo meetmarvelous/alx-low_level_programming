ffc33db0687db6ec6e885b280655540dc7c9cb03
Marvelous <marvel1698@gmail.com>
2022-09-09 00:48:20 +0100

    add alx-low_level_programming 🔥

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+/*
+ * File_name: 4-puts.c 
+ *Author: Afolabi
+ * Function: Prints a string
+ *
+ */
+
+
+#include <stdio.h>
+
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line. 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+/*
+ *File: 5-printf.c
+ *Author: Afolabi
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints with proper grammar, but the outcome is a piece of art,
+ *
+ *Return: Always returns 0
+ */
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}

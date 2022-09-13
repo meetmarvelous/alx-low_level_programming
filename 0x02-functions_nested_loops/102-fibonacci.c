87f4ea369a63adc331015337d19d6793f5dcd630
MeetMarvelous <marvel1698@gmail.com>
2022-09-14 00:12:23 +0100

    add 0x02-functions_nested_loops 🔥

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,30 +1,28 @@
-include <stdio.h>
-
+#include <stdio.h>
 /**
- * main - First fifty fibonacci numbes=rs from 1
- * Description: writes the fibonacci sequence
- * Return: Zero
+ * main - main block
+ * Description: computes and prints the sum of all the multiples of 3 or
+ * 5 below 1024 (excluded), followed by a new line
+ * Return: 0
  */
 int main(void)
 {
-	int i;
-	long int num = 1;
-	long int seq = 2;
-	long int sum = 0;
+	int i = 0;
+	long int a = 0, b = 1, next;
 
-	for (i = 1; i <= 50; i++)
+	while (i < 50)
 	{
-		if (i != 50)
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
+
+		if (i < 49)
 		{
-			printf("%ld, ", num);
-			sum = num + seq;
-			num = seq;
-			seq = sum;
+			printf(", ");
 		}
-		else
-		{
-			printf("%ld\n", num);
-		}
+		i++;
 	}
+	putchar('\n');
 	return (0);
 }
